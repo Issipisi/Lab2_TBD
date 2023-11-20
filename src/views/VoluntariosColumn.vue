c3965cd53273c02f528770d48757c51aa341870a
LudwingCabello <64160146+LudwingCabello@users.noreply.github.com>
2023-11-19 20:22:21 -0400

    Update VoluntariosColumn.vue

--- src/views/VoluntariosColumn.vue
+++ src/views/VoluntariosColumn.vue
@@ -1,9 +1,9 @@
 <template>
-  <div style="min-height: 100vh; display: flex; justify-content: center; align-items: center;">
-    <div style="background-color: #fff; padding: 1rem; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); width: 20rem;">
-      <h2 style="font-size: 1.5rem; font-weight: bold; margin-bottom: 1rem;">Voluntarios</h2>
-      <ul style="list-style: none; padding: 0;">
-        <li v-for="voluntario in voluntariosFiltrados" :key="voluntario.id" style="margin-bottom: 0.5rem;">
+  <div class="page-container">
+    <div class="form-container">
+      <h2>Voluntarios</h2>
+      <ul class="voluntarios-list">
+        <li v-for="voluntario in voluntariosFiltrados" :key="voluntario.id">
           {{ voluntario.nombre }}
         </li>
       </ul>
@@ -11,29 +11,66 @@
   </div>
 </template>
 
-
 <script>
 import axios from "axios";
+
 export default {
-    data() {
-        return {
-            voluntarios: []
-        };
+  data() {
+    return {
+      voluntarios: [],
+    };
+  },
+  computed: {
+    voluntariosFiltrados() {
+      // Aquí debes implementar la lógica para filtrar los voluntarios según tus necesidades
+      // En este ejemplo, simplemente se devuelve la lista completa de voluntarios.
+      return this.voluntarios;
     },
-    computed: {
-      voluntariosFiltrados() {
-          return this.voluntarios.filter(voluntario => parseInt(voluntario.idtarea) === parseInt(this.tareaid));
-    }},
-    created() {
-        this.tareaid = this.$route.params.tareaId;
-        // Realiza una solicitud a la API para obtener todos los voluntarios
-        axios.get('http://localhost:8080/voluntarios')
-            .then(response => {
-                this.voluntarios = response.data;
-            })
-            .catch(error => {
-                console.error('Error al obtener los voluntarios:', error);
-            });
-    }
+  },
+  created() {
+    // Realiza una solicitud a la API para obtener todos los voluntarios
+    axios
+        .get("http://localhost:8080/voluntarios")
+        .then((response) => {
+          this.voluntarios = response.data;
+        })
+        .catch((error) => {
+          console.error("Error al obtener los voluntarios:", error);
+        });
+  },
 };
 </script>
+
+<style scoped>
+.page-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: url('@/assets/imagen5.jpg') center/cover no-repeat;
+}
+
+.form-container {
+  max-width: 400px;
+  padding: 20px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+h2 {
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+
+.voluntarios-list {
+  list-style: none;
+  padding: 0;
+}
+
+.voluntarios-list li {
+  margin-bottom: 0.5rem;
+}
+</style>
