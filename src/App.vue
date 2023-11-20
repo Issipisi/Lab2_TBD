722d630cda0a0d7c7021ca8d55d0263f6d8b7c23
LudwingCabello <64160146+LudwingCabello@users.noreply.github.com>
2023-11-19 20:26:56 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,84 @@
 <template>
-  <div>
-      <router-link class='router-link' to="/creartarea">Crear Tarea</router-link>
-      <router-link class='router-link' :to="'/actualizar-tarea/' + id">Actualizar Tarea</router-link>
-      <router-link class='router-link' to="/login">Iniciar Sesión</router-link>
-      <router-link class='router-link' to="/register">Registrarse</router-link>
-      <router-link class='router-link' to="/emergencias">Emergencias</router-link>
+  <div class="app-container">
+    <nav class="navigation">
+      <router-link to="/" class="back-to-home">Volver al Inicio</router-link>
+      <router-link class="nav-btn" to="/creartarea">Crear Tarea</router-link>
+      <router-link class="nav-btn" :to="'/actualizar-tarea/' + id">Actualizar Tarea</router-link>
+      <router-link class="nav-btn" to="/login">Iniciar Sesión</router-link>
+      <router-link class="nav-btn" to="/register">Registrarse</router-link>
+      <router-link class="nav-btn" to="/emergencias">Emergencias</router-link>
+    </nav>
     <router-view />
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      id: null,  // Asegúrate de inicializarla con el valor adecuado o como null
+      id: 1,  // Asegúrate de inicializarla con el valor adecuado o como null
       // Otras propiedades de data...
     };
   },
   // Otras secciones del componente...
 };
 </script>
+
 <style>
-.router-link{
-  margin: 25px;
-  font-weight: 500;
-  color: black;
-  text-decoration: none;
-  font-size: 21px;
-  border-bottom: solid 1px;
+.app-container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
-.router-link:hover{
-  opacity: 0.3;
+
+.back-to-home {
+  display: block;
+  margin-top: 20px;
+  text-align: center;
+  text-decoration: none;
+  color: #007bff;
+}
+
+.back-to-home:hover {
+  text-decoration: underline;
+}
+.navigation {
+  background-color: #333;
+  overflow-x: auto; /* Permite el desplazamiento horizontal */
+  white-space: nowrap; /* Evita que los botones se apilen verticalmente */
+  text-align: center;
+}
+
+
+.nav-btn {
+  display: inline-block; /* Alinea los botones en una fila */
+  margin: 10px;
+  padding: 10px 15px;
+  font-weight: 500;
+  color: white;
+  text-decoration: none;
+  font-size: 18px;
+  background-color: #555;
+  border: none;
+  border-radius: 4px;
+  transition: background-color 0.3s, transform 0.2s, box-shadow 0.2s;
+  cursor: pointer;
+}
+
+.nav-btn:hover, .nav-btn:focus {
+  background-color: #666;
+  transform: translateY(-2px);
+  box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+}
+
+/* Estilos para el enlace activo */
+.router-link-active {
+  border-bottom: 3px solid #42b983;
+}
+
+/* Estilos adicionales para mejorar la responsividad */
+@media (max-width: 768px) {
+  .nav-btn {
+    padding: 8px 10px;
+    font-size: 16px;
+  }
 }
 </style>
